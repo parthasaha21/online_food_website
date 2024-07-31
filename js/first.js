fcd5e9c4b319c13100fe38793f3632ea5fed2f0b
Partha Saha <parthasaha1234567@gmail.com>
2024-07-31 20:39:56 +0530

    first commit

---
+++ js/first.js
@@ -0,0 +1,15 @@
+// script.js
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    var navLinks = document.querySelectorAll('.nav-link');
+
+    navLinks.forEach(function(link) {
+        link.addEventListener('click', function() {
+            navLinks.forEach(function(nav) {
+                nav.classList.remove('active');
+            });
+            this.classList.add('active');
+        });
+    });
+});
+
